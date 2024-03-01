ee52953abfad132022104774688eac9699f4a899
RebeccaVanLitsenborg <rebeccavanlitsenborg@gmail.com>
2024-03-01 11:10:08 +0100

    Set up components folder and Create component for InputField

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
-
+import weatherBg from './Assets/weatherBg.jpg'
+import InputField from './Components/InputField';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-    <p>Hello World</p>
-      </header>
-    </div>
+    <div className="App" style={{ backgroundImage: `url(${weatherBg})`}}>
+       <div class="overlay">
+         <div class="container">
+            <h1>Weather Forecast</h1>
+            <InputField />
+            </div>
+        </div>
+       </div>
   );
 }
 
---
+++ src/Components/InputField.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const InputField = () => {
+  return (
+    <div class="section section__InputField">
+        <input type="text" name="city" placeholder="Search by City..."></input>
+        <button>°F</button>
+    </div>
+  )
+}
+
+export default InputField
